f0db96b5c38f10262246c8366b59fbf304c73f31
Anvay Panguluri <69991035+aspanguluri@users.noreply.github.com>
2023-09-15 00:55:32 -0400

    Update login/forms.py

--- contact_book/login/forms.py
+++ contact_book/login/forms.py
@@ -16,6 +16,4 @@ class LoginForm(forms.Form):
     '''
     username = forms.CharField(label = 'username', max_length=200)
     password = forms.CharField(label = 'password', widget=forms.PasswordInput)
-
-# class DeleteContactForm(forms.Form):
-#
+    
