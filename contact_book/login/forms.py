e88865bb825d2210cf13d8f24c52499ef8c3e284
Anvay Panguluri <69991035+aspanguluri@users.noreply.github.com>
2023-09-18 17:59:47 -0400

    Update forms.py

--- contact_book/login/forms.py
+++ contact_book/login/forms.py
@@ -5,15 +5,6 @@ from django import forms
 from .models import Login
 
 class LoginForm(forms.Form):
-    '''
-    class Meta:
-        model = Login
-        fields = '__all__'
-        widgets = {
-            'username' : TextInput(attrs={"type":"text"}),
-            'password' : PasswordInput(attrs={"type":"password"})
-        }
-    '''
     username = forms.CharField(label = 'username', max_length=200)
     password = forms.CharField(label = 'password', widget=forms.PasswordInput)
     
